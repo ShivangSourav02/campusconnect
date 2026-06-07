8d4ca0a222c6a2aef4a7770bcaa0fbed6a500090
ShivangSourav02 <shivangsourav@gmail.com>
2026-06-07 10:45:36 +0530

    Add Vercer origin to CORS config

--- src/main/java/com/campusconnect/security/SecurityConfig.java
+++ src/main/java/com/campusconnect/security/SecurityConfig.java
@@ -27,7 +27,10 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:5173"));
+                    config.setAllowedOrigins(List.of(
+                            "http://localhost:5173",
+                            "https://campusconnect-frontend-nine.vercel.app"
+                    ));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
